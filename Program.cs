13fa26933bc06a4f5549214e4712bc02f278f62c
Graham Galloway <Grahamgalloway0@gmail.com>
2022-07-27 12:16:05 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+        }
+    }
+}
